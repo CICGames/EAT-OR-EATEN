e1dfafcefe596e5878ba53ebe404bd8462fe1231
dudgns0908 <k2110235@hanmail.net>
2016-05-13 02:21:59 +0900

    Commit
    Commit

---
+++ Assets/Script/PlayerSyncPosition.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerSyncPosition : NetworkBehaviour {
+
+    [SyncVar]
+    private Vector3 syncPos;
+
+    [SerializeField]
+    Transform myTransform;
+
+    [SerializeField]
+    float lerpRate = 15;
+
+    void FixedUpdate() {
+        TransmitPosition();
+        LerpPosition();
+    }
+
+    void LerpPosition() {
+        if (!isLocalPlayer) {
+            myTransform.position = Vector3.Lerp(myTransform.position, syncPos, Time.deltaTime * lerpRate);
+        }
+    }
+
+    [Command]
+    void CmdProvidePostionToServer(Vector3 pos) {
+        syncPos = pos;
+    }
+
+    [ClientCallback]
+    void TransmitPosition() {
+        if (isLocalPlayer) {
+            CmdProvidePostionToServer(myTransform.position);
+        }
+    }
+
+}
