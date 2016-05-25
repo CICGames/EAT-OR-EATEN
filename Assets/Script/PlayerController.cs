56042e2be135c2b266976187959b6cdee9736207
Daniel Choi <cydp0127@gmail.com>
2016-05-25 20:38:03 +0900

    modified player control
    modified player control

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -3,24 +3,32 @@ using System.Collections;
 using UnityEngine.Networking;
 
 public class PlayerController : NetworkBehaviour {
+    float speed;
+    private Rigidbody rb;
+    MeshCollider meshcollider;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        meshcollider = GetComponent<MeshCollider>();
+        speed = 500f;
+    }
+
+    // Update is called once per frame
+    void Update() {
 
         if (!isLocalPlayer) {
             return;
         }
 
-        float x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        float z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        if (Input.GetKey(KeyCode.W))
+            rb.AddForce(Vector3.forward * Time.deltaTime * speed);
+        if (Input.GetKey(KeyCode.S))
+            rb.AddForce(-Vector3.forward * Time.deltaTime * speed);
+        if (Input.GetKey(KeyCode.A))
+            rb.AddForce(-Vector3.right * Time.deltaTime * speed);
+        if (Input.GetKey(KeyCode.D))
+            rb.AddForce(Vector3.right * Time.deltaTime * speed);
 
-        transform.Rotate(0, x, 0);
-        transform.Translate(0, 0, z);
-
-	}
+    }
 }
