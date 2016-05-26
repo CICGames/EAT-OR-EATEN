b39917a1f17ddf7a89e3f4d63d5cd343d51f0746
Daniel Choi <cydp0127@gmail.com>
2016-05-26 17:25:04 +0900

    add pickup
    Add pickup rotator and generate it at random position

---
+++ Assets/Script/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+    int pickup_count = 0;
+    // Update is called once per frame
+    void Update() {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+}
+
