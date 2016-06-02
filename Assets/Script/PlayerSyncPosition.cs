52da14b87d8b132b8f75f8df62bbd0c34c26e152
dudgns0908 <k2110235@hanmail.net>
2016-06-02 20:24:00 +0900

    Player Scale Sync
    Player Scale Sync

--- Assets/Script/PlayerSyncPosition.cs
+++ Assets/Script/PlayerSyncPosition.cs
@@ -7,8 +7,8 @@ public class PlayerSyncPosition : NetworkBehaviour {
 
     [SyncVar] private Vector3 syncPosition = Vector3.zero;
     [SyncVar] private Quaternion syncRatation = Quaternion.identity;
-
-    [SerializeField] Rigidbody myRigidbody;
+    [SyncVar] private Vector3 syncScale = Vector3.zero;
+    
     [SerializeField] Transform myTransfrom;
   
     [SerializeField] float lerpRate = 15;
@@ -20,21 +20,23 @@ public class PlayerSyncPosition : NetworkBehaviour {
 
     private void LerpMove() {
         if (!isLocalPlayer) { // 플레이어 자신이 아닌 다른 플레이어들 움직임
-            myTransfrom.position = Vector3.Lerp(myRigidbody.position, syncPosition, Time.deltaTime * lerpRate);
+            myTransfrom.position = Vector3.Lerp(transform.position, syncPosition, Time.deltaTime * lerpRate);
             myTransfrom.rotation = Quaternion.Lerp(transform.rotation, syncRatation, Time.deltaTime * lerpRate);
+            myTransfrom.localScale = Vector3.Lerp(transform.localScale, syncScale, Time.deltaTime * lerpRate);
         }
     }
 
     [Command] // 클라이언트에서 서버로 전송? 적용?
-    void CmdProvidePostionToServer(Vector3 pos, Quaternion rot) {
+    void CmdProvidePostionToServer(Vector3 pos, Quaternion rot, Vector3 scale) {
         syncPosition = pos;
         syncRatation = rot;
+        syncScale = scale;
     }
 
     [ClientCallback] // 클라이언트에서만 동장
     private void TransMoveInformation() {
         if (isLocalPlayer) {
-            CmdProvidePostionToServer(myTransfrom.position, myTransfrom.rotation);
+            CmdProvidePostionToServer(myTransfrom.position, myTransfrom.rotation, myTransfrom.localScale);
         }
     }
 }
