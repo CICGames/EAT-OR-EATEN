e57477ab48686f22b31e8df486be8e2e6bed3692
Daniel Choi <cydp0127@gmail.com>
2016-06-17 21:27:55 +0900

    testtetes
    test

---
+++ Assets/Script/SphereRotator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereRotator : MonoBehaviour {
+
+    int speed;
+
+    Vector3 direction;
+
+
+    void Start() {
+        speed = Random.Range(100, 180);
+        float angle = Vector3.Angle(new Vector3(1, 0, 0), transform.position);
+        if (transform.localPosition.y < 0) { 
+            angle *= -1;
+        }
+        if (transform.localPosition.x < 0) {
+            angle *= -1;
+        }
+        Debug.Log(angle);
+        direction = new Vector3(angle*2, 360, 0);
+    }
+
+
+    void Update() {
+        transform.RotateAround(transform.parent.position, direction, speed *Time.deltaTime);
+    }
+}
